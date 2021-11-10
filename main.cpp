eae754e8044620fbf755f4d9ca212f08cf0b1ad2
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-11-10 21:16:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0 , x=1 ,y=1 , z=0;
+    cin >> n;
+    if(n>0){
+        cout << x << endl;
+    }
+    while(x<=n){
+            x=y+z;
+            z=y;
+            y=x;
+            if(x<=n){
+                cout << x << endl;
+            }
+    }
+    return 0;
 }
